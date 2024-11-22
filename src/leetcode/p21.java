993fcafe1ce8b1e21d611f3b7f385d371755e277
Wu-Clone <wuli77917@gmail.com>
2024-11-22 20:16:59 +0800

    p21 合并有序链表 链表 简单

---
+++ src/leetcode/p21.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class p21 {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode dummy = new ListNode();
+//        ListNode x = dummy;
+    // 这里能够优化，&& -》|| 就OK
+//        while (list1 != null && list2 != null) {
+//            if (list1.val < list2.val) {
+//                x.next = list1;
+//                list1 = list1.next;
+//            } else {
+//                x.next = list2;
+//                list2 = list2.next;
+//            }
+//            x = x.next;
+//        }
+//        x.next = (list1 != null) ? list1 : list2; // 拼接剩余部分
+//        return dummy.next;
+//    }
+
+}
