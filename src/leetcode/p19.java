86d82f72c880a44413ff8f15d068892a4d03fc34
Wu-Clone <wuli77917@gmail.com>
2024-11-22 23:23:29 +0800

    p19 删除链表倒数第n个节点 链表 中等

---
+++ src/leetcode/p19.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class p19 {
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        ListNode dummy = new ListNode(0,head);
+//        ListNode l = dummy;
+//        ListNode r = dummy;
+//        for(int i =0; i<n;i++){
+//            r = r.next;
+//        }
+//        while(r.next!=null){
+//            l = l.next;
+//            r=  r.next;
+//        }
+//        l.next = l.next.next;
+//        return dummy.next;
+//    }
+}
