5bbbf0a6ad8881fc080d7e643d8a82d791e24aae
Wu-Clone <wuli77917@gmail.com>
2024-11-23 21:17:46 +0800

    p101 对称二叉树 树 简单

---
+++ src/leetcode/p101.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class p101 {
+//    public boolean isSymmetric(TreeNode root) {
+//        return (root == null) || (root.left == null && root.right == null) || compare(root.left, root.right);
+//    }
+//
+//    private boolean compare(TreeNode l, TreeNode r) {
+//        if (l == null && r == null)
+//            return true;
+//        if (l != null && r != null)
+//            return (l.val == r.val) && compare(l.left, r.right) && compare(l.right, r.left);
+//        return false;
+//    }
+}
