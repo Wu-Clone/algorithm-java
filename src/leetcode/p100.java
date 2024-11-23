15892eee878107c2b04a86f33e2091cefb5d9da4
Wu-Clone <wuli77917@gmail.com>
2024-11-23 20:43:41 +0800

    p100 相同的树 树 简单 p104 树的最大深度 树 简单

---
+++ src/leetcode/p100.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class p100 {
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p!=null && q!=null)
+//            return (p.val == q.val)&& (isSameTree(p.left,q.left)&&isSameTree(p.right,q.right));
+//        return p==null && q==null;
+//    }
+}
