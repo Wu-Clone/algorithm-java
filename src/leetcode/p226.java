759e96e51b4b62553b22531822027064fee97cd6
Wu-Clone <wuli77917@gmail.com>
2024-11-23 20:53:56 +0800

    p226 翻转二叉树 树 简单

---
+++ src/leetcode/p226.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class p226 {
+//    public TreeNode invertTree(TreeNode root) {
+//        reverse(root);
+//        return root;
+//    }
+//
+//    private void reverse(TreeNode r){
+//        if(r==null) return;
+//        TreeNode tmp = r.left;
+//        r.left = r.right;
+//        r.right = tmp;
+//        reverse(r.right);
+//        reverse(r.left);
+//    }
+}
