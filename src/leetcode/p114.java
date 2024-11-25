746ddc35777165ebc67ac4e38f2a1602e134051b
Wu-Clone <wuli77917@gmail.com>
2024-11-26 00:24:29 +0800

    p114 二叉树展开为链表 树 中等

---
+++ src/leetcode/p114.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class p114 {
+//    public void flatten(TreeNode root) {
+//        List<TreeNode> l = new ArrayList<>();
+//        pre(root, l);
+//        TreeNode node = root;
+//        for (int i = 1; i < l.size(); i++) {
+//            node.left = null;
+//            node.right = l.get(i);
+//            node = node.right;
+//        }
+//
+//    }
+//
+//    public void pre(TreeNode node, List<TreeNode> l) {
+//        if (node == null) return;
+//        l.add(node);
+//        if (node.left != null) pre(node.left, l);
+//        if (node.right != null) pre(node.right, l);
+//    }
+}
