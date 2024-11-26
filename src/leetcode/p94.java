19a35cf2c8a85c72a276e40c42e240451cc1ab3a
Wu-Clone <wuli77917@gmail.com>
2024-11-26 15:29:56 +0800

    p94 中序遍历 简单

---
+++ src/leetcode/p94.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class p94 {
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> l = new ArrayList<>();
+//        inorder(root,l);
+//        return l;
+//    }
+//    private void inorder(TreeNode node, List<Integer> l){
+//        if(node==null) return;
+//        inorder(node.left, l);
+//        l.add(node.val);
+//        inorder(node.right,l);
+//    }
+}
