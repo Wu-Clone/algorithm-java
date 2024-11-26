0b5f757b2ba062ee8feb38a0f520e4a7500bd67a
Wu-Clone <wuli77917@gmail.com>
2024-11-26 22:22:27 +0800

    p98 验证二叉搜索树 二叉树 中等

---
+++ src/leetcode/p98.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class p98 {
+//    public boolean isValidBST(TreeNode root) {
+//        List<Integer> helper = new ArrayList<>();
+//        inOrder(root, helper);
+//        for (int i = 1; i < helper.size(); i++) {
+//            if (helper.get(i) <= helper.get(i - 1))
+//                return false;
+//        }
+//        return true;
+//    }
+//
+//    private void inOrder(TreeNode node, List<Integer> ls) {
+//        if (node == null)
+//            return;
+//        inOrder(node.left, ls);
+//        ls.add(node.val);
+//        inOrder(node.right, ls);
+//    }
+}
