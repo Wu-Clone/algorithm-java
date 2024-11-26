8d29d474a0ac08b567e1a8d51080c2c4af35e848
Wu-Clone <wuli77917@gmail.com>
2024-11-26 15:45:21 +0800

    p530 还有点疑惑

---
+++ src/leetcode/p530.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class p530 {
+//    public int getMinimumDifference(TreeNode root) {
+//        List<Integer> r = new ArrayList<>();
+//        pre(root,r);
+//        int delta = r.get(1) - r.get(0);
+//        for(int i =1;i<r.size()-1;i++){
+//            delta = Math.min(delta, r.get(i+1)-r.get(i));
+//        }
+//        return delta;
+//    }
+//    private void pre(TreeNode node, List<Integer> r){
+//        if(node==null) return;
+//        pre(node.left,r);
+//        r.add(node.val);
+//        pre(node.right,r);
+//    }
+}
