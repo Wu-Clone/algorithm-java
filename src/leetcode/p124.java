7c15416fbddbd1d65b717302cec40b72c7ee3bf0
Wu-Clone <wuli77917@gmail.com>
2024-11-30 13:44:29 +0800

    最大路径和 困难 验证数独 中等

---
+++ src/leetcode/p124.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class p124 {
+//    private int road = Integer.MIN_VALUE;
+//    public int maxPathSum(TreeNode root) {
+//        int res = nodeMax(root);
+//        return Math.max(road,res);
+//    }
+//    private int nodeMax(TreeNode node){
+//        if(node == null) return 0;
+//        int left = nodeMax(node.left);
+//        int right = nodeMax(node.right);
+//        int side = Math.max(left,right) + node.val;
+//        int treeSide = left + right + node.val;
+//        road = Math.max(road, side);
+//        road = Math.max(road, treeSide);
+//        road = Math.max(road, node.val);
+//        return Math.max(side,node.val);
+//    }
+}
