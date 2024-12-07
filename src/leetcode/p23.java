e23d2f0b4a5935d0c147f18526e6fde8760ec977
Wu-Clone <wuli77917@gmail.com>
2024-12-07 22:03:52 +0800

    p23 合并K个升序链表 链表 困难

---
+++ src/leetcode/p23.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class p23 {
+//    public ListNode mergeKLists(ListNode[] lists) {
+//        PriorityQueue<ListNode> pq = new PriorityQueue<>((n1, n2) -> (n1.val - n2.val));
+//        ListNode dummy = new ListNode();
+//        ListNode p = dummy;
+//        for (int i = 0; i < lists.length; i++) {
+//            if (lists[i] != null)
+//                pq.add(lists[i]);
+//        }
+//        while (!pq.isEmpty()) {
+//            ListNode node = pq.poll();
+//            p.next = node;
+//            if (node.next != null)
+//                pq.add(node.next);
+//            p = p.next;
+//            p.next = null;
+//        }
+//        return dummy.next;
+//    }
+}
