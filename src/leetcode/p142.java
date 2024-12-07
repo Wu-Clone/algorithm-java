efb87faa380a7319afe988c7cf222e46012d9e82
Wu-Clone <wuli77917@gmail.com>
2024-12-08 00:24:11 +0800

    p142 环形链表 中等

---
+++ src/leetcode/p142.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class p142 {
+//    public ListNode detectCycle(ListNode head) {
+//        ListNode slow = head, fast= head;
+//        if(head==null || head.next== null) return null;
+//        while(fast!=null && fast.next!=null){
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if(fast==slow) break;
+//        }
+//        if(fast==null|| fast.next==null) return null;
+//        slow = head;
+//        while(slow!=fast){
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+//        return slow;
+//    }
+}
