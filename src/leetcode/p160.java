c0b1f449fd6b83531af3feb2fe734ca6a794349d
Wu-Clone <wuli77917@gmail.com>
2024-12-07 22:46:30 +0800

    p160 相交链表 简单

---
+++ src/leetcode/p160.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class p160 {
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        ListNode nodeA = headA, nodeB = headB;
+//        while (nodeA != nodeB) {
+//            nodeA = nodeA == null ? headB : nodeA.next;
+//            nodeB = nodeB == null ? headA : nodeB.next;
+//        }
+//        return nodeA;
+//    }
+}
