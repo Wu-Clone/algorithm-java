744f76e0be8aed37b20b99c6ac60ebbbb2d3b1e0
Wu-Clone <wuli77917@gmail.com>
2024-12-07 22:27:23 +0800

    p876 链表的中间节点 链表 简单

---
+++ src/leetcode/p876.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class p876 {
+//    public ListNode middleNode(ListNode head) {
+//        // ListNode dummy = new ListNode(-1,head);
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        while(fast!=null && fast.next!=null ){
+//            fast = fast.next.next;
+//            slow = slow.next;
+//        }
+//        return slow;
+//    }
+}
