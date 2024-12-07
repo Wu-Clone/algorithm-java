a7d0bbc72386941870045ecca912f3ce7c47b067
Wu-Clone <wuli77917@gmail.com>
2024-12-08 01:31:47 +0800

    p142 删除有序链表中的重复元素 简单

---
+++ src/leetcode/p83.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class p83 {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        ListNode dummy = new ListNode(-101,head);
+//        ListNode p = dummy;
+//        while(p!= null){
+//            while(p.next!=null && p.val==p.next.val){
+//                p.next = p.next.next;
+//            }
+//            p = p.next;
+//        }
+//        return dummy.next;
+//    }
+}
