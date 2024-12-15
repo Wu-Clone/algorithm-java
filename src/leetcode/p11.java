cf278592883be8de6719ee6a1dab6509154b23f8
Wu-Clone <wuli77917@gmail.com>
2024-12-15 23:20:51 +0800

    p11 代码更新

--- src/leetcode/p11.java
+++ src/leetcode/p11.java
@@ -5,19 +5,16 @@ public class p11 {
     //如果当前两根柱子中较短的一根被移动，它有可能被更高的柱子代替，从而有机会形成更大的面积。
     //如果移动较高的柱子，新的面积只会更小或相等，因为面积受短板限制（木桶效应）。
     public int maxArea(int[] height) {
-        int l = 0, r= height.length-1;
-        // 容器体积
-        int vul = 0;
+        int res = 0;
+        int l = 0, r = height.length - 1;
         while (l < r) {
-            int mid = Math.min(height[r], height[l]);
-            vul = Math.max(vul, (r - l) * mid);
-            while (mid >= height[l] && l<r) {
+            int mid = Math.min(height[l], height[r]);
+            res = Math.max(mid * (r - l), res);
+            while (l < r && mid >= height[l])
                 l++;
-            }
-            while (mid >= height[r] && l<r) {
+            while (l < r && mid >= height[r])
                 r--;
-            }
         }
-        return vul;
+        return res;
     }
 }
